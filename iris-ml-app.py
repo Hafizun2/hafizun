65a711c3352756cfc1f32b7508d21c8188e86a19
Hafizun2 <126454515+Hafizun2@users.noreply.github.com>
2023-03-07 14:39:55 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -12,14 +12,14 @@ This app predicts the **Iris flower** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
+    TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
+    Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
+    Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
+    Sales = st.sidebar.slider('Sales', 0.1, 2.5, 0.2)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper,
+            'Sales': Sales}
     features = pd.DataFrame(data, index=[0])
     return features
 
